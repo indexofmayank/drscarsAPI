05c15aa2d70d1227d065fa46946ba98b79c8240d
officailRscars <officail@rscars.in>
2023-04-19 14:47:29 +0530

    'nodemailerdone'

--- routes/booking-routes.js
+++ routes/booking-routes.js
@@ -4,55 +4,76 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Errorhandler = require("../utils/errorhander");
 const router = express.Router();
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
-
-router.post(`/`, catchAsyncErrors(async(req, res, next) => {
+router.post(
+  `/`,
+  catchAsyncErrors(async (req, res, next) => {
     let booking = new Booking({
-        name: req.body.name,
-        startDate: req.body.startDate,
-        startTime: req.body.startTime,
-        endDate: req.body.endDate,
-        endTime: req.body.endTime,
-        pricePerDay: req.body.pricePerDay,
-        totalPrice: req.body.totalPrice,
-        altEmail: req.body.altEmail,
-        phone: req.body.phone
+      name: req.body.name,
+      startDate: req.body.startDate,
+      startTime: req.body.startTime,
+      endDate: req.body.endDate,
+      endTime: req.body.endTime,
+      pricePerDay: req.body.pricePerDay,
+      totalPrice: req.body.totalPrice,
+      altEmail: req.body.altEmail,
+      phone: req.body.phone,
     });
 
     booking = await booking.save();
 
-    if(!booking) {
-        return next(new Errorhandler("Something Went Wrong"));
+    if (!booking) {
+      return next(new Errorhandler("Something Went Wrong"));
     }
 
-    res.status(201).json({
-        success: true,
-        booking
+    console.log(booking.altEmail)
+
+    const transporter = nodemailer.createTransport({
+      host: "mail.rscars.in",
+      port: 465,
+      auth: {
+        user: "official@rscars.in",
+        pass: "ET(pX10HYR*o",
+      },
+    });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"rscars 👻" <official@rscars.in>', // sender address
+      to: booking.altEmail, // list of receivers
+      subject: "New Booking  ✔", // Subject line
+      text: JSON.stringify(booking), // plain text body
     });
 
-}));
+    res.status(201).json({
+      success: true,
+      booking,
+      info,
+    });
+  })
+);
 
-
-router.get(`/`, catchAsyncErrors(async(req, res, next) => {
+router.get(
+  `/`,
+  catchAsyncErrors(async (req, res, next) => {
     const bookings = await Booking.find();
 
-    if(!bookings) {
-        return next (new Errorhandler("No Bookings found at this moment"));
+    if (!bookings) {
+      return next(new Errorhandler("No Bookings found at this moment"));
     }
 
     res.status(201).json({
-        success: true,
-        bookings
+      success: true,
+      bookings,
     });
-}));
+  })
+);
 
-
-
-
-
-
-router.get(`/test`, catchAsyncErrors(async ( req, res, next) => {
+router.get(
+  `/test`,
+  catchAsyncErrors(async (req, res, next) => {
     res.send("Working from Booking-routes");
-}));
+  })
+);
 
 module.exports = router;
